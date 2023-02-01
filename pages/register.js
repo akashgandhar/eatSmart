7429dc9820e8e5e830447426954ff89cc99f32de
akashgandhar <akashgandhar007@gmail.com>
2023-02-01 13:30:44 +0530

    login register

--- pages/register.js
+++ pages/register.js
@@ -8,63 +8,67 @@ import React, { use, useContext, useState } from "react";
 
 export default function Register() {
   const router = useRouter();
-  const [name, setName] = useState("null");
-  const [email, setEmail] = useState("null");
-  const [password, setPassword] = useState("null");
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
   const u = useContext(UserContext)
   const [eye, setEye] = useState(false);
 
   const handlesignup = (e) => {
     e.preventDefault();
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed in
-        const user = userCredential.user;
-        // setUser(user);
-        updateProfile(user, {
-          displayName: name,
-        })
-          .then(async () => {
-            try {
-              await setDoc(doc(db, "users", email), {
-                Name: name,
-                Email: email,
-              });
-            } catch (e) {
-              alert(e.message);
-            }
+    try {
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          // setUser(user);
+          updateProfile(user, {
+            displayName: name,
           })
-          .then(() => {
-            alert("success");
-            router.push("/login");
-          });
+            .then(async () => {
+              try {
+                await setDoc(doc(db, "users", email), {
+                  Name: name,
+                  Email: email,
+                });
+              } catch (e) {
+                alert(e.message);
+              }
+            })
+            .then(() => {
+              alert("success");
+              router.push("/login");
+            });
 
-        // ...
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // ..
-        alert(errorMessage);
-      });
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          // ..
+          alert(errorMessage);
+        });
+    } catch (e) {
+      alert(e.message)
+    }
   };
 
   return (
     <div className="w-full h-screen ">
       <section className="">
         <div class="px-4 py-12 mx-auto max-w-7xl sm:px-6 md:px-12 lg:px-24 lg:py-24">
-          <div class="justify-center mx-auto text-left align-bottom transition-all transform bg-white rounded-lg sm:align-middle sm:max-w-2xl sm:w-full">
+          <div class="justify-center mx-auto text-left align-bottom transition-all transform bg-white rounded-lg sm:align-middle sm:max-w-2xl sm:w-full ">
             <div class="grid flex-wrap items-center justify-center grid-cols-1 mx-auto shadow-xl lg:grid-cols-2 rounded-xl">
-              <div class="m-auto w-full ">
+              <div class="m-auto w-full h-full ">
                 <div class="rounded-xl w-full bg-white shadow-xl">
                   <div class="p-6 sm:p-16 ">
                     <div class="space-y-4 ">
-                      <img
+                      {/* <img
                         src="/lg.png"
                         // loading="lazy"
                         class="w-20 m-0"
                         alt="tailus logo"
-                      />
+                      /> */}
                       <div class="mb-6 mx-2 text-2xl text-cyan-900 font-bold">
                         Sign Up
                         <h1 className="mt-3"> </h1>
@@ -93,7 +97,7 @@ export default function Register() {
                           </label> */}
                         <input
                           onChange={(e) => {
-                            setName(e.target.value);
+                            setEmail(e.target.value);
                           }}
                           type="text"
                           name="name"
@@ -108,14 +112,35 @@ export default function Register() {
                           </label> */}
                         <input
                           onChange={(e) => {
-                            setName(e.target.value);
+                            setPassword(e.target.value);
                           }}
-                          type="text"
+                          type="password"
                           name="name"
                           id="email"
                           class="block w-full px-5 py-3 text-base text-neutral-900  transition duration-500 ease-in-out transform border border-transparent rounded-lg bg-gray-200 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300"
                           placeholder="Password"
                         />
+                      </div>
+                      <div className="w-full flex justify-center items-center">
+                        <Button onClick={(e) => {
+                          console.log("clicked");
+                          if (!name || !email || !password) {
+                            alert("Enter Details")
+                          }
+                          else {
+
+
+                            if (email.includes("@")) {
+                              handlesignup(e)
+                            }else{
+                              alert(
+                                "email wrong"
+                              )
+                            }
+                          }
+                        }} variant="outlined" color="success">
+                          Sign Up
+                        </Button>
                       </div>
 
                     </div>
